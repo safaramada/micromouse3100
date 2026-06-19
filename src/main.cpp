dfdb9b5f931af687f381c058ad02ff6c0cf37ea0
Jason Yang <z5477733@ad.unsw.edu.au>
2026-06-19 21:18:57 +1000

    Jason: added lab3 code into the folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,130 +1,37 @@
 #include <Arduino.h>
-// #include <Wire.h>
-// #include <VL6180X.h>
 
-/*
-    Key Responsibilities:       
-        - Setup sensors / motors
-        - call task functions
-        - task functions use motor/sensor helper 
+const int MOT1_DIR = 10;
+const int MOT1_PWM = 9;
 
-
-    // Week 4 Task 
-        Drive forward 200 mm accurately (+- 75mm)
-        Rotate in place - 4 90 degree turns counter clockwise 
-        4 90 degree turns clockwise 
-        After each of these iterations it should be generally facing original direction
-        
-*/
-
-// ----- Constants -----
-const int DRIVE_SPEED = 150;       
-const int TURN_SPEED = 140;    
-
-
-
-
-// ----- Function -----
-void setupMotors();
-void setMotorSpeeds(int leftSpeed, int rightSpeed);
-
-
-void driveForwardMM(int distanceMM);
-void turnLeft90();
-void turnRight90();
-void stopMotors();
+const int MOT2_DIR = 12;
+const int MOT2_PWM = 11;
 
 void setup() {
   Serial.begin(9600);
-  setupMotors();
 
-  delay(1000); // Give time to place robot after powering on.
+  pinMode(MOT1_DIR, OUTPUT);
+  pinMode(MOT1_PWM, OUTPUT);
+  pinMode(MOT2_DIR, OUTPUT);
+  pinMode(MOT2_PWM, OUTPUT);
 
-  // Task 1: drive 200 mm, then stop.
-  driveForwardMM(200);
-  stopMotors();
-
-  delay(1000);
-
-  // Task 2a: four 90 degree counter-clockwise turns.
-  for (int i = 0; i < 4; i++) {
-    turnLeft90();
-    stopMotors();
-    delay(300);
-  }
-
-  delay(1000);
-
-  // Task 2b: four 90 degree clockwise turns.
-  for (int i = 0; i < 4; i++) {
-    turnRight90();
-    stopMotors();
-    delay(300);
-  }
-
-  stopMotors();
+  Serial.println("Motor repeat test started");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 void loop() {
-  // Week 4 task only runs once in setup().
-}
+  Serial.println("Motors forward");
 
-void setupMotors() {
- 
-  stopMotors();
-}
+  digitalWrite(MOT1_DIR, LOW);
+  digitalWrite(MOT2_DIR, LOW);
 
-void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+  analogWrite(MOT1_PWM, 255);
+  analogWrite(MOT2_PWM, 255);
 
- 
-}
+  delay(3000);
 
-void driveForwardMM(int distanceMM) {
+  Serial.println("Motors off");
 
+  analogWrite(MOT1_PWM, 0);
+  analogWrite(MOT2_PWM, 0);
 
-
-  int driveTimeMS = distanceMM * MS_PER_MM;
-
-  setMotorSpeeds(DRIVE_SPEED, DRIVE_SPEED);
-  delay(driveTimeMS);
-  stopMotors();
-}
-
-void turnLeft90() {
-
-
-
-  setMotorSpeeds(-TURN_SPEED, TURN_SPEED);
-  delay(100); // Replace with constant after testing.
-  stopMotors();
-}
-
-void turnRight90() {
-
-
-
-  setMotorSpeeds(TURN_SPEED, -TURN_SPEED);
-  delay(100); // Replace with constant after testing.
-  stopMotors();
-}
-
-void stopMotors() {
-
-
-
-  setMotorSpeeds(0, 0);
+  delay(2000);
 }
