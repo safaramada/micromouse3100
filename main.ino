cdebcd0ad2649255252ab61812ded50e688cec1d
Jason Yang <z5477733@ad.unsw.edu.au>
2026-07-03 16:51:37 +1000

    update pins for encoders

--- main.ino
+++ main.ino
@@ -14,22 +14,31 @@
 
 #define LEFT_MOTOR_PWM 9 // PIN 9 is a PWM pin
 #define LEFT_MOTOR_DIR 10
-#define RIGHT_MOTOR_PWM 6 // TODO: Update this pin to match your robot
-#define RIGHT_MOTOR_DIR 7 // TODO: Update this pin to match your robot
+#define RIGHT_MOTOR_PWM 11 // TODO: Update this pin to match your robot
+#define RIGHT_MOTOR_DIR 12 // TODO: Update this pin to match your robot
 mtrn3100::Motor left_motor(LEFT_MOTOR_PWM, LEFT_MOTOR_DIR);
 mtrn3100::Motor right_motor(RIGHT_MOTOR_PWM, RIGHT_MOTOR_DIR);
 
+
+
+
 #define LEFT_EN_A 2 // PIN 2 is an interrupt
-#define LEFT_EN_B 4
+#define LEFT_EN_B 7
 #define RIGHT_EN_A 3 // PIN 3 is an interrupt
-#define RIGHT_EN_B 5 // TODO: Update this pin to match your robot
+#define RIGHT_EN_B 8 // TODO: Update this pin to match your robot
 mtrn3100::Encoder left_encoder(LEFT_EN_A, LEFT_EN_B);
 mtrn3100::Encoder right_encoder(RIGHT_EN_A, RIGHT_EN_B);
+
+
+
+
 
 mtrn3100::Lidar front_lidar;
 mtrn3100::Lidar left_lidar;
 mtrn3100::Lidar right_lidar;
 mtrn3100::IMU imu;
+
+
 mtrn3100::Robot robot(left_motor, right_motor, left_encoder, right_encoder, front_lidar, left_lidar, right_lidar, imu);
 
 const char command_string[] = "lfrfflfr";
