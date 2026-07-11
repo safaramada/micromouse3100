2b73ab6cfe0809d171bbf68e2b5694090db80bdf
Safa Ramadanny <safa.ramadanny@gmail.com>
2026-07-11 12:47:39 +1000

    yey

--- include/StraightLineTracking.hpp
+++ include/StraightLineTracking.hpp
@@ -3,80 +3,23 @@
 #include <Arduino.h>
 #include "Robot.hpp"
 
+namespace mtrn3100 {
+
 class StraightLineTracking {
 public:
     StraightLineTracking(Robot& robot) : robot(robot) {}
 
     void begin() {
-        robot.encoders.reset();
-
-        delay(1000); // keep still while IMU settles
-
-        targetYaw = robot.imu.getYaw();
-
-        Serial.print("Target yaw: ");
-        Serial.println(targetYaw);
-
-        finished = false;
+        // Slightly more than 1m so it clearly passes the finish line
+        robot.startStraightLine(1050.0, 140);
     }
 
     void update() {
-        if (finished) {
-            robot.stopMotors();
-            return;
-        }
-
-        float currentYaw = robot.imu.getYaw();
-        float yawError = angleError(currentYaw, targetYaw);
-
-        float correction = Kp * yawError;
-
-        int leftPWM  = baseLeftPWM  - correction;
-        int rightPWM = baseRightPWM - correction;
-
-        leftPWM = constrain(leftPWM, 80, 180);
-        rightPWM = constrain(rightPWM, -180, -80);
-
-        robot.leftMotor.setPWM(leftPWM);
-        robot.rightMotor.setPWM(rightPWM);
-
-        float distanceMM = robot.encoders.getAverageDistanceMM();
-
-        Serial.print("Yaw: ");
-        Serial.print(currentYaw);
-        Serial.print(" Error: ");
-        Serial.print(yawError);
-        Serial.print(" Distance: ");
-        Serial.println(distanceMM);
-
-        if (distanceMM >= 1000) {
-            robot.stopMotors();
-            finished = true;
-        }
+        robot.update();
     }
 
 private:
     Robot& robot;
-
-    float targetYaw = 0;
-    bool finished = false;
-
-    float Kp = 4.0;
-
-    int baseLeftPWM = 155;
-    int baseRightPWM = -150;
-
-    float angleError(float current, float target) {
-        float error = current - target;
-
-        while (error > 180) {
-            error -= 360;
-        }
-
-        while (error < -180) {
-            error += 360;
-        }
-
-        return error;
-    }
 };
+
+}
