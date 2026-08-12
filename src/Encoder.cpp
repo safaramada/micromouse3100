cd95a033228b053c45330e820e3b1a931f5e2c98
nileyar <n.yar@student.unsw.edu.au>
2026-08-13 01:49:34 +1000

    Add encoder static definitions

---
+++ src/Encoder.cpp
@@ -0,0 +1,10 @@
+// AI-assisted maintenance: one definition here avoids header ODR violations.
+
+#include "Encoder.hpp"
+
+namespace mtrn3100 {
+
+Encoder* Encoder::instances[2] = {nullptr, nullptr};
+uint8_t Encoder::instance_count = 0;
+
+}  // namespace mtrn3100
